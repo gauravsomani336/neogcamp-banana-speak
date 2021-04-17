7ec37ece71fb153a9bf7e35ac9d43e3a0133ccde
Gaurav somani <gauravsomani336@gmail.com>
2021-04-17 22:51:02 +0530

    final banana app without styling

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output")
 console.log(txtInput)
 
+var serverUrl="https://api.funtranslations.com/translate/yoda.json";
+
+function generateUrl(text)
+{
+    return serverUrl+"?"+"text="+text
+}
+function errorHandler(error)
+{
+    console.log("error occured",error)
+    alert("something wrong occured")
+}
 function clickEventHandler() {
-    outputDiv.innerText="akakakakak" + txtInput.value;
+    var inputText=txtInput.value;
+
+    fetch(generateUrl(inputText))
+    .then(response=>response.json())
+    .then(json=> {
+        var translatedtext1=json.contents.translated;
+        outputDiv.innerText=translatedtext1;
+    })
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickEventHandler)
